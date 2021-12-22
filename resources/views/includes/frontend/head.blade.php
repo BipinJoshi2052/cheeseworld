9e9a39c6c55aa20bcb3b22a1fde2fb5c82447fa7
sovitmaharjan <sovitmaharjan@gmail.com>
2021-12-22 15:47:05 +0545

    home page shop page

--- resources/views/includes/frontend/head.blade.php
+++ resources/views/includes/frontend/head.blade.php
@@ -1,7 +1,32 @@
 <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>Cheese World</title>
+<!-- Bootstrap link Starts -->
+<link rel="stylesheet" href="frontend/assets/bootstrap-4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="frontend/assets/bootstrap-4.3.1/css/bootstrap.min.css.map">
+<!-- Bootstrap link Ends -->
+<!-- Font Awesome Link Starts -->
+<link rel="stylesheet" href="frontend/assets/font-awesome-4.7.0/css/font-awesome.min.css">
+<!-- Font Awesome Link Ends -->
+<!-- Slick Css -->
+<link rel="stylesheet" href="frontend/assets/slick/slick.css">
+<link rel="stylesheet" href="frontend/assets/slick/slick-theme.css">
+<!-- Slick Css Ends-->
+<!-- Custom Links -->
+<!-- Font Link -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700;800&family=DM+Serif+Display:ital@0;1&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&family=Vidaloka&display=swap" rel="stylesheet">
+<!-- Font Link Ends -->
+<link rel="stylesheet" href="frontend/assets/css/style.css">
+<link rel="stylesheet" href="frontend/assets/css/responsive.css">
+
+
+{{-- <meta charset="UTF-8">
 <title>@yield('title')</title>
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-<meta name="csrf-token" content="{{ csrf_token() }}">
 
 @if((Request::is('product/details/*') || Request::is('product/customize/*')) && !empty($single_product_details['meta_keywords']))
 <meta name="keywords" content="{{ $single_product_details['meta_keywords'] }}">
@@ -137,4 +162,4 @@
 <script type="text/javascript" src="{{ URL::asset('public/slick/slick.min.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('public/common/base64.js') }}"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
-<script src="{{ URL::asset('public/js-socials/jssocials.min.js') }}"></script>
+<script src="{{ URL::asset('public/js-socials/jssocials.min.js') }}"></script> --}}
